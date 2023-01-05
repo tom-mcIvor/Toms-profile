fc7c2a536c448939c94bf918c8331315996adf5c
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2023-01-06 04:55:37 +1300

    add

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
-
+import Todo from './Todo'
 
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+
+
+<>
+  <Component {...pageProps}/>
+
+  <Todo />
+    
+</>
+
+   
+   )
 }
