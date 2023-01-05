af461af102830c0f5f2afa28bf024c271cdac42c
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2023-01-06 04:22:21 +1300

    add

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app
+  .prepare()
+  .then(() => {
+    const server = express()
+
+    // Add your server-side routes here
+    // For example:
+    // server.get('/api/todos', (req, res) => {
+    //   res.json([
+    //     { id: 1, text: 'Buy milk' },
+    //     { id: 2, text: 'Write code' },
+    //   ])
+    // })
+
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
+
+    server.listen(3000, err => {
+      if (err) throw err
+      console.log('> Ready on http://localhost:3000')
+    })
+  })
+  .catch(ex => {
+    console.error(ex.stack)
+    process.exit(1)
+  })
