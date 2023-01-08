5d96a5bba69ac7ae5824225ab52f70beb59438a8
tom mcivor <ttmcivor@gmail.com>
2023-01-09 11:31:02 +1300

    add

--- pages/components/Home.jsx
+++ pages/components/Home.jsx
@@ -4,7 +4,6 @@ const Welcome = () => {
   return (
     <header>
       <h1>Tom&apos;s profile</h1>
-      <p>Hi, I&#39;m Tom and I am a skilled web developer with experience in technologies like Git, React, Redux, CSS, and JavaScript. I am currently seeking new job opportunities and would love to bring my passion for web development to your organization. When I&#39;m not coding, you can find me eating pasta and playing social tennis. Thank you for visiting my website!</p>
     </header>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -35,6 +35,9 @@ export default function Home() {
             </div>
             <DarkModeToggle/>
       </main>
+      <div id="about">
+        <p>Hi, I&#39;m Tom and I am a skilled web developer with experience in technologies like Git, React, Redux, CSS, and JavaScript. I am currently seeking new job opportunities and would love to bring my passion for web development to your organization. When I&#39;m not coding, you can find me eating pasta and playing social tennis. Thank you for visiting my website!</p>
+      </div>
     </>
   )
 }
