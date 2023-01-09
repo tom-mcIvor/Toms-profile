fc12108e81024548e9a2b76641295336c98339c3
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2023-01-10 08:02:14 +1300

    add

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
-const path = require('path')
 const express = require('express')
+const next = require('next')
+const server = express();
+const { parse } = require('url')
+const { createServer } = require('http')
+const dev = process.env.NODE_ENV !== 'production'
+const hostname = 'localhost'
+const port = 3000
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port })
+const handle = app.getRequestHandler()
 
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      // Be sure to pass `true` as the second argument to `url.parse`.
+      // This tells it to parse the query portion of the URL.
+      const parsedUrl = parse(req.url, true)
+      const { pathname, query } = parsedUrl
 
-const tennisRoutes = require('./tennis')
-
-
-const server = express()
-
-server.use(express.json())
-server.use(express.static(path.join(__dirname, 'public')))
-
-
-server.use('/api/v1/tennis', tennisRoutes)
-
+      if (pathname === '/a') {
+        await app.render(req, res, '/a', query)
+      } else if (pathname === '/b') {
+        await app.render(req, res, '/b', query)
+      } else {
+        await handle(req, res, parsedUrl)
+      }
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err)
+      res.statusCode = 500
+      res.end('internal server error')
+    }
+  }).listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://${hostname}:${port}`)
+  })
+})
 
 module.exports = server
