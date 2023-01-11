805e856f2b582f35fef4e8f0f8399b98f350f5ea
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2023-01-12 05:57:29 +1300

    add

--- pages/components/Tennis.jsx
+++ pages/components/Tennis.jsx
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import Layout from '../../components/Layout';
-
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { useDispatch, useSelector } from 'react-redux'
 
 const Tennis = () => {
   const [data, setData] = useState(null);
+  const [playerId, setPlayerId] = useState('');
+  const [showImage, setShowImage] = useState(false);
+
   const options = {
     method: 'GET',
     headers: {          
@@ -13,18 +19,31 @@ const Tennis = () => {
   };
 
   useEffect(() => {
+    fetch(`https://tennisapi1.p.rapidapi.com/api/tennis/player/${playerId}/image`, options)
+        .then(response => response.blob())
+        .then(response => { console.log(response); setData(response) })
+        .catch(err => console.error(err));
+    
 
-    fetch('https://tennisapi1.p.rapidapi.com/api/tennis/player/14486/image', options)
-      .then(response => response.blob())
-      .then(response => { console.log(response); setData(response) })
-      .catch(err => console.error(err));
+  }, [playerId]);
 
-  }, []);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(`https://tennisapi1.p.rapidapi.com/api/tennis/player/${playerId}/image`, options)
+        .then(response => response.blob())
+        .then(response => { console.log(response); setData(response) })
+        .catch(err => console.error(err));
+        e => setPlayerId(e.target.value)
+        setShowImage(true)
+  }
 
   return (
     <Layout>
     <div>
-      {data && <img src={URL.createObjectURL(data)} alt={data.alt} />}
+    <TextField label="Search Player" variant="outlined" value={playerId} onChange={e => setPlayerId(e.target.value)} />
+      { showImage && data && <img src={URL.createObjectURL(data)} alt={data.alt} />}
+      <br />
+      <Button variant="contained" color="primary" type="submit" onClick={handleSubmit}>Submit</Button>
     </div>
     </Layout>
   );
