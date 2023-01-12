5f0633047074be00b52577f2ad678ac474caf15f
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2023-01-13 04:40:34 +1300

    add

--- pages/components/Tennis.jsx
+++ pages/components/Tennis.jsx
@@ -11,7 +11,7 @@ const Tennis = () => {
   const [showImage, setShowImage] = useState(false);
   const [loading, setLoading] = useState(false);
   const [playerName, setPlayerName] = useState('');
-  
+
   const options = {
     method: 'GET',
     headers: {
@@ -20,62 +20,111 @@ const Tennis = () => {
     }
   };
 
-  const handleSubmit = (e) => {
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setLoading(false);
+
+  // fetch(`https://tennisapi1.p.rapidapi.com/api/tennis/search/${playerName}`, options)
+  // .then(response => response.json())
+  // .then(response => {
+  //   setPlayerId(response.results[0].entity.id)
+  //   setLoading(false);
+  //     })
+  // .catch(err => console.error(err));
+
+  // fetch(`https://tennisapi1.p.rapidapi.com/api/tennis/player/${playerId}/image`, options)
+  //   .then(response => { 
+  //     return (response.blob())
+  //     })
+  //   .then(response => {
+  //     if (!response) {
+  //       setLoading(true);
+  //   }
+  //     setData(response)
+  //     setShowImage(true)
+  //   })
+  //   .catch(error => {
+  //     console.error(error);
+  //     setLoading(true);
+  //   });
+
+  //     // e.preventDefault();
+  //     // setLoading(false);
+
+  //     // fetch(`https://tennisapi1.p.rapidapi.com/api/tennis/search/${playerName}`, options)
+  //     // .then(response => response.json())
+  //     // .then(response => {
+  //     //   setPlayerId(response.results[0].entity.id)
+  //     //   setLoading(false);
+
+  //     // return(
+
+  //     // fetch(`https://tennisapi1.p.rapidapi.com/api/tennis/player/${playerId}/image`, options)
+  //     //   .then(response => { 
+  //     //     return (response.blob())
+  //     //     })
+  //     //   .then(response => {
+  //     //     if (!response) {
+  //     //       setLoading(true);
+  //     //   }
+  //     //     setData(response)
+  //     //     setShowImage(true)
+  //     //   })
+  //     //   .catch(error => {
+  //     //     console.error(error);
+  //     //     setLoading(true);
+  //     //   })
+  //     //   )
+  //     // })
+  // }
+
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(false);
-    
-    fetch(`https://tennisapi1.p.rapidapi.com/api/tennis/search/${playerName}`, options)
-    .then(response => response.json())
-    .then(response => {
-      setPlayerId(response.results[0].entity.id)
-      setLoading(false);
-        })
-    .catch(err => console.error(err));
-    
-    fetch(`https://tennisapi1.p.rapidapi.com/api/tennis/player/${playerId}/image`, options)
-      .then(response => { 
-        return (response.blob())
-        })
-      .then(response => {
-        if (!response) {
-          setLoading(true);
-      }
-        setData(response)
-        setShowImage(true)
-      })
-      .catch(error => {
-        console.error(error);
+    try {
+      const response = await fetch(`https://tennisapi1.p.rapidapi.com/api/tennis/search/${playerName}`, options);
+      const jsonData = await response.json();
+      if (jsonData.results.length > 0) {
+        setPlayerId(jsonData.results[0].entity.id);
         setLoading(true);
-      });
+        const imageResponse = await fetch(`https://tennisapi1.p.rapidapi.com/api/tennis/player/${playerId}/image`, options);
+        const imageBlob = await imageResponse.blob();
+        setData(imageBlob);
+        setShowImage(true);
+        setLoading(false);
+      }
+    } catch (err) {
+      console.error(err)
+      setLoading(true)
+    }
   }
 
   return (
-    <Layout> 
+    <Layout>
       <Box onSubmit={handleSubmit}
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
       >
         <div id='tennis-form'>
-          <TextField 
+          <TextField
             label="Search Player"
-            variant="outlined" 
-            // value={playerId}
+            variant="outlined"
             value={playerName}
-            // onChange={(e) => setPlayerId(e.target.value)}
-             onChange={(e) => setPlayerName(e.target.value)}
+            onChange={(e) => setPlayerName(e.target.value)}
           />
         </div>
-          <Button 
-            variant="contained"
-            color="primary"
-            type="submit"
-            >Submit</Button>
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+        >Submit</Button>
       </Box>
-            {loading ? <p>error...</p> : showImage && data && <img src={URL.createObjectURL(data)} alt={data.alt} onLoad={() => setLoading(false)}  />}
+      {loading ? <p>loading...</p> : showImage && data && <img src={URL.createObjectURL(data)} alt={data.alt} onLoad={() => setLoading(false)} />}
     </Layout>
   )
 }
