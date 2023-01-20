fa4e2fbb45f4ac3240903648023f1dbfd40f5ca7
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2023-01-20 21:17:20 +1300

    add

---
+++ models/db.js
@@ -0,0 +1,17 @@
+const config = require('./knexfile').development
+const connection = require('knex')(config)
+
+function getTodos(db = connection) {
+  return db('todos').select()
+}
+
+// Your DB functions go here
+
+function close(db = connection) {
+  db.destroy()
+}
+
+module.exports = {
+  getTodos,
+  close,
+}
---
+++ models/migrations/20230120051531_todos.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('todos', (table)=>{
+    table.increments('id')
+    table.string('task')
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('todos')
+};
