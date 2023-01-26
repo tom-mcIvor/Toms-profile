772fa39d2ff374502b9108a9c765d25a1cf1df32
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2023-01-27 01:24:15 +1300

    add

--- models/knexfile.js
+++ models/knexfile.js
@@ -4,47 +4,49 @@ const path = require('path')
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-
   development: {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: path.join(__dirname, 'dev.sqlite3')
-    }
+      filename: path.join(__dirname, 'dev.sqlite3'),
+    },
+    migrations: {
+      directory: path.join(__dirname, '../migrations'),
+    },
+    seeds: {
+      directory: path.join(__dirname, '../seeders'),
+    },
   },
-  
-  staging: {  
+
+  staging: {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
-      password: 'password'
+      user: 'username',
+      password: 'password',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: 'knex_migrations',
+    },
   },
 
   production: {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
-      password: 'password'
+      user: 'username',
+      password: 'password',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-  
-};
-
-
+      tableName: 'knex_migrations',
+    },
+  },
+}
