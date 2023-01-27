4413ecaa2ae3616516b77975e831600cce5473d2
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2023-01-27 16:24:32 +1300

    add

--- models/knexfile.js
+++ models/knexfile.js
@@ -12,22 +12,6 @@ module.exports = {
     }, 
   },
 
-  staging: {
-    client: 'sqlite3',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
-  },
-
   production: {
     client: 'sqlite3',
     connection: {
